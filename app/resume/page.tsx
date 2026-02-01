88b2f632987e54929e2a14ff22d6a14a06e88e88
RChaubey16 <ruturajchaubey16@gmail.com>
2026-02-01 13:19:42 +0530

    feat: add resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,9 @@
+import ResumeViewer from "@/components/resume/ResumeViewer";
+
+export default function Home() {
+  return (
+    <section className="pt-20 bg-background">
+      <ResumeViewer />
+    </section>
+  );
+}
--- components/About/Profile.tsx
+++ components/About/Profile.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import { BriefcaseBusiness, Clock8, Eye, Mail, MapPin } from "lucide-react";
+import {
+  BriefcaseBusiness,
+  Clock8,
+  Eye,
+  FileUser,
+  Mail,
+  MapPin,
+  Send,
+} from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
 import RoleSlider from "../animation/Roles";
 import InfoIconCard from "./InfoIconCard";
 import SocialCard from "../SocialCard";
 import newConfig from "@/data/newConfig.json";
 import { getIndiaTimeLabel } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function Profile() {
   const {
@@ -27,7 +37,7 @@ export default function Profile() {
       <div className="mt-20 flex flex-col items-center">
         <div className="flex flex-col items-center">
           {/* Image */}
-          <div className="relative w-25 h-25 rounded-md border border-chart-3">
+          <div className="relative w-25 h-25 rounded-md border border-manatee">
             <Image
               src={avatarImageUrl}
               alt={avatarImageAltText}
@@ -83,18 +93,43 @@ export default function Profile() {
         />
       </div> */}
 
-      {socials && (
-        <div className="mt-2 mb-6 flex justify-center flex-wrap gap-2">
-          {socials.map((item) => (
-            <SocialCard
-              key={item.name}
-              name={item.name}
-              logoSrc={item.logoSrc}
-              href={item.href}
-            />
-          ))}
+      <div className="flex flex-col md:flex-row gap-3 justify-between items-center">
+        <div className="flex items-center gap-2">
+          <Link href={"/contact"} target="_blank">
+            <Button
+              variant="default"
+              size={"default"}
+              className="flex items-center gap-2 cursor-pointer transition-all ease-in-out duration-200 hover:gap-4"
+            >
+              Get In Touch
+              <Send />
+            </Button>
+          </Link>
+          <Link href={"/resume"} className="">
+            <Button
+              variant="outline"
+              size={"default"}
+              className="flex items-center gap-2 cursor-pointer transition-all ease-in-out duration-200 hover:gap-4"
+            >
+              My Resume
+              <FileUser />
+            </Button>
+          </Link>
         </div>
-      )}
+
+        {socials && (
+          <div className="flex justify-center flex-wrap gap-2">
+            {socials.map((item) => (
+              <SocialCard
+                key={item.name}
+                name={item.name}
+                logoSrc={item.logoSrc}
+                href={item.href}
+              />
+            ))}
+          </div>
+        )}
+      </div>
 
       <div className="mt-4">
         <h2 className="section-title">About</h2>
